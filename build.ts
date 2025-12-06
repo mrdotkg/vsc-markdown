0b789da577a578bced536de4c6fd10d145ea7664
Kr <grv.rkg@gmail.com>
2025-12-06 12:49:50 +0530

    build script clean up

--- build.ts
+++ build.ts
@@ -40,29 +40,8 @@ function main() {
     })
 }
 
-function createLib() {
-    const points = dependencies.reduce((point, dependency) => {
-        const main = require(`./node_modules/${dependency}/package.json`).main ?? "index.js";
-        const mainAbsPath = resolve(`./node_modules/${dependency}`, main);
-        if (existsSync(mainAbsPath)) {
-            point[dependency] = mainAbsPath;
-        }
-        return point;
-    }, {})
-    build({
-        entryPoints: points,
-        bundle: true,
-        outdir: "out/node_modules",
-        format: 'cjs',
-        platform: 'node',
-        minify: true,
-        treeShaking: true,
-        metafile: true
-    })
-}
 
 (async () => {
-    createLib();
     main();
     if (isProd) {
         setTimeout(() => process.exit(0), 100);
